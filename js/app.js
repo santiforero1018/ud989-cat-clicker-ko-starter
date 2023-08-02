aa273a5cbe5a48a3e7505609d0dcc4dc08dd67ff
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-08-01 20:41:51 -0500

    practicando con KO.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@ var ViewModel = function (){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Michi');
     this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.level = ko.observable('NewBorn');
 
     this.incrementCounter = function (){
         this.clickCount(this.clickCount() + 1);
+        this.improveCat();
     };
+
+    this.improveCat = function (){
+        if(this.clickCount() == 10){
+            this.level('Infant');
+        }
+        else if(this.clickCount() == 100){
+            this.level('Teen');
+        }
+    }
 }
 
 ko.applyBindings(new ViewModel());
