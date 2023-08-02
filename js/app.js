908dc10a4c09081f304aedc75c34671597b55d46
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-08-02 18:36:14 -0500

    commit antes del refactor para añadir mas gatos

--- js/app.js
+++ js/app.js
@@ -1,22 +1,43 @@
-var ViewModel = function (){
+var Cat = function (){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Michi');
     this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-    this.level = ko.observable('NewBorn');
+    this.nickNames = ko.observable([{name: 'Michi'},
+                                    {name: 'Mushi'},
+                                    {name: 'Bigotes'}, 
+                                    {name:  'Tilin'}, 
+                                    {name: 'Silvestre'}]);
+    this.level = ko.computed(function (){
+        var title, 
+        clicks = this.clickCount();
+        if (clicks < 10){
+            title = 'NewBorn';
+        } else if (clicks < 50){
+            title = 'Infant';
+        } else if (clicks < 100){
+            title = 'Child';
+        } else if (clicks < 200){
+            title = 'Teen';
+        } else if (clicks < 500){
+            title = 'Adult';
+        } else {
+            title = 'Ninja';
+        }
+
+        return title;
+    }, this);
+    
+}
+
+
+var ViewModel = function (){
+    var self = this; // se almacena a ViewModel, para poder trabajar con el al usar with en el html
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function (){
-        this.clickCount(this.clickCount() + 1);
-        this.improveCat();
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
-    this.improveCat = function (){
-        if(this.clickCount() == 10){
-            this.level('Infant');
-        }
-        else if(this.clickCount() == 100){
-            this.level('Teen');
-        }
-    }
-}
+};
 
 ko.applyBindings(new ViewModel());
